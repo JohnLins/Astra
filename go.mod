faa8880c713246012a982ba0409d1d784e6a41ef
Matthew Penner <me@matthewp.io>
2020-10-02 14:50:56 -0600

    Initialize go modules

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/AstraApp/server
+
+go 1.15
+
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	golang.org/x/net v0.0.0-20201002202402-0a1ea396d57c // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	fmt.Println("Astra")
+	fmt.Println("Astra Server")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	http.ListenAndServe(":8080", r)
 }
