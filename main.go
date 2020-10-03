4b4243e1d465f029dd416944ce6214c02ed858e2
John Lins <46113320+JohnLins@users.noreply.github.com>
2020-10-02 19:09:11 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,142 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/AstraApp/server/scraper"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
+/*
+type Coordinates struct {
+	latitude  int `json: "y"`
+}
+
+
+// We will move this function later, this is a placeholder
+
+func fetchSatelliteName(la, lo, z int) string {
+	fmt.Println(la, la, z)
+	//example
+	rangeGroup := 5
+
+	satellites, err := ioutil.ReadFile("./satellites.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var coordinates []Coordinates
+
+	err = json.Unmarshal(satellites, &coordinates)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//Search la and lo in json
+
+	return ""
+}
+*/
+/*
+func section(x, y, x int)int {
+	if x < 0 {
+		if y < 0 {
+			if z < 0 {
+				//
+				return
+			}
+			if z >= 0 {
+				//
+				return
+			}
+		}
+		if y >= 0 {
+			if z < 0 {
+				//
+				return
+			}
+			if z >= 0 {
+				//
+				return
+			}
+		}
+	}
+	if x >= 0 {
+		if y < 0 {
+			if z < 0 {
+				//
+				return
+			}
+			if z >= 0 {
+				//
+				return
+			}
+		}
+		if y >= 0 {
+			if z < 0 {
+				//
+				return
+			}
+			if z >= 0 {
+				//
+				return
+			}
+		}
+	}
+}
+
+*/
+
 func main() {
 	fmt.Println("Astra Server")
 
+	s := scraper.New()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		latitude, err := strconv.Atoi(r.URL.Query().Get("latitude"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		longitude, err := strconv.Atoi(r.URL.Query().Get("longitude"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		zed, err := strconv.Atoi(r.URL.Query().Get("zed"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		_ = fetchSatelliteName(latitude, longitude, zed)
+
+		// TODO: Replace "GOES" with satelliteName
+		satellite, err := s.Scrape("GOES")
+		if err != nil {
+			fmt.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		b, err := json.MarshalIndent(satellite, "", "\t")
+		if err != nil {
+			fmt.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(b)
 	})
 
 	http.ListenAndServe(":8080", r)
